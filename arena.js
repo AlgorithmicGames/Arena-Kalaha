b7ac4adb12cfa75aca8e4251866d76497d7d9bb9
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2026-04-03 23:13:06 +0000

    Refactor arena.js for improved readability and consistency; update properties.json formatting; enhance replay index.html and index.js for better functionality and clarity.

--- arena.js
+++ arena.js
@@ -1,120 +1,120 @@
 'use strict'
-function isGameFinished(gameboard){
-	let ai_1 = 0;
-	let ai_2 = 0;
-	let length = gameboard.length;
-	let lengthHalf = (length/2)-1;
-	length--;
-	for(let i = 0; i < length; i++){
-		if(i === lengthHalf){
-			i++;
+function isGameFinished(gameboard) {
+	let ai_1 = 0
+	let ai_2 = 0
+	let length = gameboard.length
+	let lengthHalf = (length / 2) - 1
+	length--
+	for (let i = 0; i < length; i++) {
+		if (i === lengthHalf) {
+			i++
 		}
-		let score = gameboard[i];
-		if(i < lengthHalf){
-			ai_1 += score;
-		}else{
-			ai_2 += score;
-		}
-	}
-	return ai_1 === 0 || ai_2 === 0;
-}
-function doMove(gameboard, move, rules){
-	let size = gameboard.length;
-	let steps = gameboard[move];
-	let ownStore = (size/2)-1;
-	gameboard[move] = 0;
-	while(0 < steps){
-		move++;
-		move %= size;
-		if(move < size-1){	// If on last, do not add. Is opposite goal.
-			steps--;
-			gameboard[move] += 1;
+		let score = gameboard[i]
+		if (i < lengthHalf) {
+			ai_1 += score
+		} else {
+			ai_2 += score
 		}
 	}
-	if(rules.empty_capture){
-		if(move < ownStore && 1 === gameboard[move]){
-			let score = gameboard[move];
-			gameboard[move] = 0;
-			let oppositeSide = size - move - 2;
-			score += gameboard[oppositeSide];
-			gameboard[oppositeSide] = 0;
-			gameboard[ownStore] += score;
+	return ai_1 === 0 || ai_2 === 0
+}
+function doMove(gameboard, move, rules) {
+	let size = gameboard.length
+	let steps = gameboard[move]
+	let ownStore = (size / 2) - 1
+	gameboard[move] = 0
+	while (0 < steps) {
+		move++
+		move %= size
+		if (move < size - 1) { // If on last, do not add. Is opposite goal.
+			steps--
+			gameboard[move] += 1
 		}
 	}
-	return {moveAgain: move === ownStore, gameboard, gameboard};
-}
-function sumBoard(gameboard){
-	let data = [0,0];
-	for(let i = 0; i < gameboard.length; i++){
-		data[i < gameboard.length/2 ? 0 : 1] += gameboard[i];
+	if (rules.empty_capture) {
+		if (move < ownStore && 1 === gameboard[move]) {
+			let score = gameboard[move]
+			gameboard[move] = 0
+			let oppositeSide = size - move - 2
+			score += gameboard[oppositeSide]
+			gameboard[oppositeSide] = 0
+			gameboard[ownStore] += score
+		}
 	}
-	return data;
+	return { moveAgain: move === ownStore, gameboard, gameboard }
 }
-function sumScore(score, gameboardLength, startValue, participants){
-	let boardValue = gameboardLength*startValue;
-	let errorFound = boardValue != score[0] + score[1];
-	if(errorFound){
-		return null;
+function sumBoard(gameboard) {
+	let data = [0, 0]
+	for (let i = 0; i < gameboard.length; i++) {
+		data[i < gameboard.length / 2 ? 0 : 1] += gameboard[i]
 	}
-	participants.forEach(participant => {
-		participant.addScore(score[participant.team]);
-	});
+	return data
 }
-function callParticipant(match, aiIndex){
-	let participant = match.participants.get(aiIndex%2, 0);
-	participant.postMessage(match.gameboard).then(response => {
-		let selectedMove = 0;
-		if(response.message){
-			if(0 <= response.message.data && response.message.data < match.gameboard.length/2 && 0 < match.gameboard[response.message.data]){ // Check if legal move.
-				selectedMove = response.message.data;
+function sumScore(score, gameboardLength, startValue, participants) {
+	let boardValue = gameboardLength * startValue
+	let errorFound = boardValue != score[0] + score[1]
+	if (errorFound) {
+		return null
+	}
+	participants.forEach((participant) => {
+		participant.addScore(score[participant.team])
+	})
+}
+function callParticipant(match, aiIndex) {
+	let participant = match.participants.get(aiIndex % 2, 0)
+	participant.postMessage(match.gameboard).then((response) => {
+		let selectedMove = 0
+		if (response.message) {
+			if (0 <= response.message.data && response.message.data < match.gameboard.length / 2 && 0 < match.gameboard[response.message.data]) { // Check if legal move.
+				selectedMove = response.message.data
 			}
 		}
-		while(match.gameboard[selectedMove] === 0){ // Validate
-			selectedMove++;
+		while (match.gameboard[selectedMove] === 0) { // Validate
+			selectedMove++
 		}
-		let moveData = doMove(match.gameboard, selectedMove, match.settings.rules);
-		match.gameboard = moveData.gameboard;
-		ArenaHelper.log('tick', {mover: participant.name, gameboard: match.gameboard.slice()});
+		let moveData = doMove(match.gameboard, selectedMove, match.settings.rules)
+		match.gameboard = moveData.gameboard
+		ArenaHelper.log('tick', { mover: participant.name, gameboard: match.gameboard.slice() })
 
 		// Switch AI
-		if(!moveData.moveAgain){
-			aiIndex++;
-			for(let i=0; i < match.gameboard.length/2; i++){
-				match.gameboard.push(match.gameboard.shift());
+		if (!moveData.moveAgain) {
+			aiIndex++
+			for (let i = 0; i < match.gameboard.length / 2; i++) {
+				match.gameboard.push(match.gameboard.shift())
 			}
 		}
-		if(isGameFinished(match.gameboard)){
-			if(aiIndex%2){
-				for(let i=0; i < match.gameboard.length/2; i++){
-					match.gameboard.push(match.gameboard.shift());
+		if (isGameFinished(match.gameboard)) {
+			if (aiIndex % 2) {
+				for (let i = 0; i < match.gameboard.length / 2; i++) {
+					match.gameboard.push(match.gameboard.shift())
 				}
 			}
-			match.participants.terminateAllWorkers();
-			let score = sumScore(sumBoard(match.gameboard), match.gameboard.length-2, match.settings.gameboard.startValue, match.participants);
-			if(score === null){
-				ArenaHelper.postAbort(participant, 'General error - Illegal final score.');
-			}else{
-				ArenaHelper.postDone();
+			match.participants.terminateAllWorkers()
+			let score = sumScore(sumBoard(match.gameboard), match.gameboard.length - 2, match.settings.gameboard.startValue, match.participants)
+			if (score === null) {
+				ArenaHelper.postAbort(participant, 'General error - Illegal final score.')
+			} else {
+				ArenaHelper.postDone()
 			}
-		}else{
-			callParticipant(match, aiIndex);
+		} else {
+			callParticipant(match, aiIndex)
 		}
-	});
+	})
 }
 ArenaHelper.init = (participants, settings) => {
-	let gameboard = [];
-	for(let i = 0; i < 2; i++){
-		for(let n=0; n < settings.gameboard.boardLength; n++){
-			gameboard.push(settings.gameboard.startValue);
+	let gameboard = []
+	for (let i = 0; i < 2; i++) {
+		for (let n = 0; n < settings.gameboard.boardLength; n++) {
+			gameboard.push(settings.gameboard.startValue)
 		}
-		gameboard.push(0);
+		gameboard.push(0)
 	}
 	let match = {
 		participants: null,
 		score: undefined,
 		gameboard: gameboard,
-		settings: settings
-	};
-	match.participants = participants;
-	callParticipant(match, 0);
+		settings: settings,
+	}
+	match.participants = participants
+	callParticipant(match, 0)
 }
